f4676ad8fbc92203b3e9117f3721fd7c9cffe2cc
zannew <zannewone@gmail.com>
2020-04-17 08:10:25 +0900

    Create Quiz_15.java

---
+++ java_project/Assignment/src/cond_and_loop_2/Quiz_15.java
@@ -0,0 +1,34 @@
+package cond_and_loop_2;
+
+public class Quiz_15 {
+	public static void main(String[] args) {
+		
+		
+		int number = 12321;
+		int tmp = number;
+		int result =0; // 변수 number를 거꾸로 변환해서 담을 변수
+
+		while(tmp !=0) {
+			for (int i = 1; i < number; i*=10) {
+				result+=(tmp%10)*i;
+				tmp=tmp-result;
+				
+				System.out.println(tmp);
+		}
+			
+			
+			
+			
+		if(number == result)
+		System.out.println( number + "는 회문수 입니다.");
+
+		else
+		System.out.println( number + "는 회문수가 아닙니다.");
+		
+		
+		if(tmp>=number)
+			break;
+		}
+		
+	}
+}
