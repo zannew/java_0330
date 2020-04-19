f91d0c2da6ae718dc9a6986f6b1ddb31f0c5b28d
zannew <zannewone@gmail.com>
2020-04-19 23:50:52 +0900

    2020-04-19
    assignment

--- java_project/Assignment/src/cond_and_loop_2/Quiz_13.java
+++ java_project/Assignment/src/cond_and_loop_2/Quiz_13.java
@@ -8,28 +8,23 @@ public class Quiz_13 {
 		char ch = ' ';
 		boolean isNumber = true;
 
-		// 반복문과 charAt(int i)를 이용해서 문자열의 문자를
+		// 반복문과 charAt(inti)를 이용해서 문자열의 문자를
 		// 하나씩 읽어서 검사한다.
 
 		for(int i=0; i < value.length() ;i++) {
 			ch = value.charAt(i);
-			int j = ch-'0';		//char → int
 			if(ch>=48 && ch<=57) {
-				isNumber = true;
 			}else {
-				isNumber = false;
+				isNumber=false;
 			}
-			System.out.println(isNumber);
 		}
-		
+
 		if (isNumber) {
 		System.out.println(value+"는 숫자입니다.");
 		} else {
 		System.out.println(value+"는 숫자가 아닙니다.");
-		}	
-		
-	}
-	
-	
-	
-}
+		}
+
+	} // end ofmain
+
+} // end ofclass
--- java_project/Assignment/src/cond_and_loop_2/Quiz_15.java
+++ java_project/Assignment/src/cond_and_loop_2/Quiz_15.java
@@ -8,20 +8,11 @@ public class Quiz_15 {
 		int tmp = number;
 		int result = 0; // 변수 number를 거꾸로 변환해서 담을 변수
 
-		while(tmp !=0) {
-			for (int i = 1; i < number; i*=10) {
-					
-				System.out.println(i);
-				result=tmp%(10*i);
-				System.out.println(tmp);
-				System.out.println(result);
+		while(tmp!=0) {
+			result = (result*10)+(tmp%10);
+			tmp=tmp/10;
+			
 		}
-			System.out.println(number);
-			
-			if(tmp>=number)
-				return;
-		
-			
 		if(number == result)
 		System.out.println( number + "는 회문수 입니다.");
 
@@ -29,7 +20,6 @@ public class Quiz_15 {
 		System.out.println( number + "는 회문수가 아닙니다.");
 		
 		
-		}
 		
 	}
 }
