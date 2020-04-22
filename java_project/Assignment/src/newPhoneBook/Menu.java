1b40261735b1ea3165f9afda7620f8695ccf3007
Yoonwon Chang <zannewone@gmail.com>
2020-04-22 20:20:02 +0900

    day17
    day17

--- java_project/Assignment/src/newPhoneBook/Menu.java
+++ java_project/Assignment/src/newPhoneBook/Menu.java
@@ -5,14 +5,14 @@ public class Menu {
 	
 	public static void showMenu() {
 		
-		System.out.println("---------------------");
+		System.out.println("===================");
 		System.out.println("번호를 선택하세요.");
 		System.out.println("1. 친구 정보 저장");
 		System.out.println("2. 친구 정보 검색");
 		System.out.println("3. 친구 정보 삭제");
 		System.out.println("4. 친구 정보 전체보기");
 		System.out.println("5. 프로그램 종료");
-		System.out.println("---------------------");
+		System.out.println("===================");
 		
 		
 		
