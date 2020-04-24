ae473990f4f0cf0553abcbf5a204d2a18cebbb00
Yoonwon Chang <zannewone@gmail.com>
2020-04-24 18:22:00 +0900

    day19
    day19

--- java_project/Assignment/src/phoneNumBook_ver_4_1/PhoneCompanyInfor.java
+++ java_project/Assignment/src/phoneNumBook_ver_4_1/PhoneCompanyInfor.java
@@ -5,7 +5,6 @@ public class PhoneCompanyInfor extends PhoneInfo {
 	String company;
 	
 	
-	
 	public PhoneCompanyInfor(String name, String phoneNum, String address, String email, String company) {
 		super(name, phoneNum, address, email);
 		this.company=company;
