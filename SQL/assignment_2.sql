ed470f0c8776bb9036a645bc7b8fbb7c7223e418
zannew <zannewone@gmail.com>
2020-05-20 22:43:50 +0900

    Update assignment_2.sql

--- SQL/assignment_2.sql
+++ SQL/assignment_2.sql
@@ -20,7 +20,7 @@ from emp
 ;
 
 --20. 올해 몇 칠이 지났는지 출력하시오. 현재날짜에서 올해 1월 1일을 뺀 결과를 출력하고 TO_DATE 함수를 사용하여 데이터 형을 일치 시키시오.
-select trunc(sysdate-to_date('20200101', 'yyyy-mm-dd')) as pastDays
+select trunc(sysdate-to_date('20200101', 'yyyy-mm-dd')) as lastDays
 from dual
 ;
 
@@ -69,29 +69,47 @@ select max(sal)-min(sal)
 from emp
 ;
 
---28. 직급별 사원의 최저 급여를 출력하시오. 관리자를 알 수 없는 사원의 최저 급여가 2000 미만인 그룹은 제외시키고 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
+--28. 직급별 사원의 최저 급여를 출력하시오. 관리자를 알 수 없는 사원과 최저 급여가 2000 미만인 그룹은 제외시키고 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
+select job, min(sal)
+from emp
+group by job, mgr
+having min(sal)>=2000 and mgr is not null
+order by min(sal) desc
+;
 
 
 --29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 소수점 둘째 자리로 반올림 하시오.
-select deptno, count(*), round(avg(sal),2)
+select deptno, count(*), round(avg(sal),2) as avgsal
 from emp
 group by deptno
 ;
 
-select *
-from dept
-;
-
 --30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 정수로 반올림 하시오. DECODE 사용.
 
-
---31. 업무job를 표시한 다음, 
---해당 업무job에 대해 부서 번호별deptno 급여sal 및 부서 10, 20, 30의 급여 총액sum을 각각 출력하시오. 
---별칭은 각 job, dno, 부서 10, 부서 20, 부서 30, 총액으로 지정하시오. ( hint. Decode, group by )
-select job, deptno, sal
+select deptno,
+decode(deptno,10,'ACCOUNTING'
+            ,20,'RESEARCH'
+            ,30,'SALES') as dname ,
+decode(deptno, 10, 'NEW YORK'
+            ,20, 'DALLAS'
+            ,30,'CHICAGO')as location,            
+            count(*)as "사원 수",
+            round(avg(sal),0) as "평균 급여"   
 from emp
-group by job, deptno, sal
+group by deptno
 ;
 
 
+--31. 업무를 표시한 다음, 
+--해당 업무에 대해 부서 번호별 급여 및 부서 10, 20, 30의 급여 총액을 각각 출력하시오. 
+--별칭은 각 job, dno, 부서 10, 부서 20, 부서 30, 총액으로 지정하시오. ( hint. Decode, group by )
+select distinct job, deptno as dno,
+decode(deptno,10, sum(sal)) as "부서10",
+decode(deptno,20, sum(sal)) as "부서20",
+decode(deptno,30,sum(sal)) as "부서30",
+sum(sal) as "총액"
+from emp
+group by job,deptno
+order by deptno
+;
 
