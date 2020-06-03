751269c96ec66f790019e8ec2add14f6475dde1c
zannew <zannewone@gmail.com>
2020-06-04 08:47:11 +0900

    Update AllDTO.java

--- java_project/Assignment/src/phoneBook_ver_7_3/AllDTO.java
+++ java_project/Assignment/src/phoneBook_ver_7_3/AllDTO.java
@@ -1,5 +1,29 @@
 package phoneBook_ver_7_3;
 
+import java.sql.Date;
+
 public class AllDTO {
 
+	private int pidx;
+	private String name;
+	private String phoneNum;
+	private String addr;
+	private String email;
+	private String fr_type;
+	private Date regdate;
+	
+	
+//	private int pidx;
+	private String major;
+	private int grade;
+	private int fr_ref;
+	
+	
+//	private int pidx;
+	private String comName;
+	private String dname;
+	private String comJob;
+	private int fr_ref;
+	
+	
 }
