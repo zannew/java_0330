2cf547585ec6cdc193679c838276512e976ff6bf
zannew <zannewone@gmail.com>
2020-12-01 22:29:42 +0900

    -

--- java_project/Cp15-Thread/src/ex/ThreadMain8.java
+++ java_project/Cp15-Thread/src/ex/ThreadMain8.java
@@ -5,6 +5,7 @@ public class ThreadMain8 {
 
 	public static void main(String[] args) {
 		
+	// TwoNum 객체를 한번만 생성해서 공유  2020/10/15
 	TwoNum tn = new TwoNum();
 	
 	AccessThread at1 = new AccessThread(tn);
@@ -15,12 +16,14 @@ public class ThreadMain8 {
 	
 	
 	try {
+		// at1의 실행이 끝날 때까지 기다린다.
 		at1.join();
+		// at2의 실행이 끝날 때까지 기다린다.
 		at2.join();
 	} catch (InterruptedException e) {
 		e.printStackTrace();
 	}
-	
+	// 그리고 마지막으로 출력메서드 실행
 	tn.showNums();
 	
 	}
